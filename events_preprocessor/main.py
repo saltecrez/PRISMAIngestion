7e374405dd0c60c7d077a2ba450bd2445ef05ced
saltecrez <3lisa.londero@gmail.com>
2020-01-13 12:51:18 +0100

    do not copy foreign stations to preprocessing area and do check size on single station folders

--- events_preprocessor/main.py
+++ events_preprocessor/main.py
@@ -14,7 +14,9 @@ import shutil
 import read_tools
 import mysql_tools
 from glob import glob
+from datetime import datetime
 from shutil import Error
+from shutil import ignore_patterns
 from folder_size import folder_size 
 from send_email import send_email
 from table_objects import data_file
@@ -57,6 +59,8 @@ else:
 #####################################
 # copy events to preprocessing area #
 #####################################
+foreign_stations_list = read_tools.read_txt(stations_file,CWD,logfile) 
+ignore_list = [i + '*' for i in foreign_stations_list] 
 for j in range(len(selected_event_list)):
 
     # find the elements in event_list that have already been archived 
@@ -64,31 +68,17 @@ for j in range(len(selected_event_list)):
     archived_event = mysql_tools.select_event(session,data_file,event_string,logfile)
 
     # if event not yet found in DB, copy to preprocessing area 
-    # calculate event folder size before and after copy 
     if not archived_event and selected_event_list[j][0:4] >= '2020': 
-	size_at_origin = folder_size(selected_event_path_list[j],logfile)
 	process_path = os.path.join(proc_path,selected_event_list[j])
-	failure_path = os.path.join(fail_path,selected_event_list[j])
 	try:
-	    shutil.copytree(selected_event_path_list[j],process_path,symlinks = True)
+	    # do not copy foreign stations to preprocessing area
+	    shutil.copytree(selected_event_path_list[j],process_path,symlinks = True,ignore=ignore_patterns(*ignore_list))
 	except shutil.Error as err:
 	    logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
-	size_at_destination = folder_size(process_path,logfile)
-
-	# compare size before and after copy
-	# if different send alert and move the folder to the failures directory
-	if size_at_origin != size_at_destination:
-	    msg = "Event alert: folder copied in " + proc_path + "not consistent with folder in " + event_path + "for event " + selected_event_list[j]
-	    send_email(msg,recipient,sender,smtp_host,logfile)
-	    try:
-	        shutil.move(process_path,failure_path)
-	    except shutil.Error as err:
-		logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
 
 #####################
 # events processing #
 #####################
-foreign_stations_list = read_tools.read_txt(stations_file,CWD,logfile) 
 events_to_process_path_list = glob(proc_path + '/*')
 
 for i in events_to_process_path_list:
@@ -98,54 +88,65 @@ for i in events_to_process_path_list:
     station_folders_path_list = glob(i + '/*/')
     station_fullnames_list = [os.path.basename(os.path.normpath(k)) for k in station_folders_path_list]
     station_names_list = [k[:-19] for k in station_fullnames_list]
-    station_names_list
 
     for j in range(len(station_names_list)):
-        # if an Italian station is found:
+	# compare size before and after copy
+	# if different send alert and move the folder to the failures directory
+	station_original_path = event_path + '/' + os.path.basename(os.path.normpath(i))[0:18] + '/' + station_fullnames_list[j]
+	size_at_origin = folder_size(station_original_path,logfile)
+	size_at_destination = folder_size(station_folders_path_list[j],logfile)
+	failure_path = os.path.join(fail_path,station_fullnames_list[j])
+	if size_at_origin != size_at_destination:
+	    msg = "Event alert: folder copied in " + proc_path + " not consistent with folder in " + event_path + " for event " + selected_event_list[j]
+	    send_email(msg,recipient,sender,smtp_host,logfile)
+	    try:
+		shutil.move(process_path,failure_path)
+	    except shutil.Error as err:
+		logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
+
         # rename its thumbnail and copy to thumbnail folder
-	if station_names_list[j] not in foreign_stations_list:
-	    thumbnail_path = glob(station_folders_path_list[j] + "/*-thumb.jpg")
-	    if thumbnail_path:
-	        try:
-		    shutil.copy(thumbnail_path[0],os.path.join(thumbs_path,station_fullnames_list[j] + '.jpg'))
-	        except IOError as err:
-		    logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
+	thumbnail_path = glob(station_folders_path_list[j] + "/*-thumb.jpg")
+	if thumbnail_path:
+	    try:
+	        shutil.copy(thumbnail_path[0],os.path.join(thumbs_path,station_fullnames_list[j] + '.jpg'))
+	    except IOError as err:
+	        logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
 
-	    # rename its FITS file and add the EVENT key
-	    # the value of the EVENT key corresponds to the event string  
-            fits_path = glob(station_folders_path_list[j] + "/*.fit")
-	    if fits_path:
-		fits_filename = os.path.basename(fits_path[0])
-		fits_path_renamed = station_folders_path_list[j] + 'Sum_' + fits_filename 
-		event_string = os.path.basename(os.path.normpath(i))[0:15]
-		os.rename(fits_path[0],fits_path_renamed)
-		fits_add_key(fits_path_renamed,'EVENT',event_string,'Event label',logfile)
-		tar_filename = station_fullnames_list[j] + '.tar.gz' 
+	# rename its FITS file and add the EVENT key
+	# the value of the EVENT key corresponds to the event string  
+        fits_path = glob(station_folders_path_list[j] + "/*.fit")
+	if fits_path:
+	    fits_filename = os.path.basename(fits_path[0])
+	    fits_path_renamed = station_folders_path_list[j] + 'Sum_' + fits_filename 
+	    event_string = os.path.basename(os.path.normpath(i))[0:15]
+	    os.rename(fits_path[0],fits_path_renamed)
+	    fits_add_key(fits_path_renamed,'EVENT',event_string,'Event label',logfile)
+	    tar_filename = station_fullnames_list[j] + '.tar.gz' 
 
-		# count number of files inside the folder
-		folder_elements = sum([len(files) for r, d, files in os.walk(station_fullnames_list[j])])
+	    # count number of files inside the folder
+	    folder_elements = sum([len(files) for r, d, files in os.walk(station_fullnames_list[j])])
 
-		# create a tar file and if successful, count the number of files contained in it
-		exit_code = create_tarfile(tar_filename,station_fullnames_list[j],logfile)
-		if exit_code == True:
-		    tar_elements = int(count_tar_elements(tar_filename,logfile))
+	    # create a tar file and if successful, count the number of files contained in it
+	    exit_code = create_tarfile(tar_filename,station_fullnames_list[j],logfile)
+	    if exit_code == True:
+		tar_elements = int(count_tar_elements(tar_filename,logfile))
 	
-		    # if the number of files in the folder equals the number of files in the tar
-		    # copy the tar to the Nadir ingestion folder
-		    # otherwise send an alert and move the tar file to the failures folder
-		    if tar_elements == folder_elements:
-		        try:
-		            shutil.copy(tar_filename,ingest_path)	
-			    logfile.write('%s -- Event ingestion: %s \n' % (datetime.now(),event_string))
-		        except IOError as err:
-			    logfile.write('%s -- IO.Error: %s \n' % (datetime.now(),err))
-		    else:
-		        msg = 'Number of frames in the tar and in the original folder do not match'
-		        send_email(msg,recipient,sender,smtp_host,logfile)
-			try:
-			    shutil.move(tar_filename,failure_path)
-			except shutil.Error as err:
-			    logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
+		# if the number of files in the folder equals the number of files in the tar
+		# copy the tar to the Nadir ingestion folder
+		# otherwise send an alert and move the tar file to the failures folder
+		if tar_elements == folder_elements:
+		    try:
+		        shutil.copy(tar_filename,ingest_path)	
+		        logfile.write('%s -- Event ingestion: %s \n' % (datetime.now(),event_string))
+		    except IOError as err:
+		        logfile.write('%s -- IO.Error: %s \n' % (datetime.now(),err))
+		else:
+		    msg = 'Number of frames in the tar and in the original folder do not match'
+		    send_email(msg,recipient,sender,smtp_host,logfile)
+		    try:
+		        shutil.move(tar_filename,failure_path)
+		    except shutil.Error as err:
+		        logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
 
     # Remove the folder in the preprocessing area
     shutil.rmtree(i)

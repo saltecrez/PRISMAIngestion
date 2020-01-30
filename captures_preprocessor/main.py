79d405cb619a5746cc0b5c85625c8fd0f3850852
saltecrez <3lisa.londero@gmail.com>
2020-01-30 21:26:01 +0100

    sqlalchemy multiprocessing

--- captures_preprocessor/main.py
+++ captures_preprocessor/main.py
@@ -13,7 +13,14 @@ import read_tools
 import mysql_tools
 from glob import glob
 from datetime import datetime
-from table_objects import data_file
+from sqlalchemy import func
+import multiprocessing
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer,select
+from base import Base
 
 CWD = os.getcwd()
 logfile = open(CWD + '/' + "logfile.txt",'a')
@@ -24,49 +31,78 @@ cnf = read_tools.read_json('conf.json',CWD,logfile)
 cameras_path = cnf['camerasfolder'];   ingest_path = cnf['ingestfolder']
 db_host      = cnf['dbhost'];          db_pwd      = cnf['dbpwd']
 db_user      = cnf['dbuser'];          db_name     = cnf['dbname']
-thumbs_path = cnf['thumbsfolder']
+tb_name      = cnf['tbname'];          thumbs_path = cnf['thumbsfolder']
+thrd_nr      = int(cnf['threadsnr']);
 
 # create captures folder list from the folder synchronized with the French server
 cameras_path_list = glob(cameras_path + '/*/')
 print cameras_path_list
 
-# create mysql database session
-Session = mysql_tools.mysql_session(db_user,db_pwd,db_host,db_name,logfile)
-session = Session()
-valid_session = mysql_tools.validate_session(session)
-if valid_session:
-   pass
-else:
-    raise Exception('The DB session could not start. Check DB credentials used in the configuration file.')
+class data_file(Base):
+    __tablename__ = 'PRS'
+
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String(45))
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+def do_work(filename):
+    t1 = datetime.now()
+    Session = mysql_tools.mysql_session(db_user,db_pwd,db_host,db_name,logfile)
+    session = Session()
+    valid_session = mysql_tools.validate_session(session)
+    if valid_session:
+        print("PID %d: using connection %s" % (os.getpid(), session))
+        rows = session.query(data_file)
+        flt = rows.filter(data_file.file_name == filename).scalar() is not None
+        session.close()
+        t2 = datetime.now()
+        delta = t2-t1
+        if not flt:
+            return filename
+        print "do work performance: ", delta
+    else:
+	raise Exception('The DB session could not start. Check DB credentials used in the configuration file.')
 
 current_month = datetime.today().strftime('%Y%m')
 cameras_month_path = [i + current_month for i in cameras_path_list]
 
 for j in range(len(cameras_month_path)):
 
-    # FITS and Thumbnails archiviation
+    # JPG
+    common_string = os.path.basename(os.path.splitext(os.path.normpath(i))[0])
+    jpg500_name = common_string + '-500x500.jpg'
+    jpg_name = common_string + '.jpg' 
+    final_destination_jpg = os.path.join(thumbs_path,jpg_name)
+    jpg_original_path = os.path.join(cameras_month_path[j],jpg500_name) 
+    if not os.path.exists(final_destination_jpg) and os.path.exists(jpg_original_path):
+        try:
+             shutil.copy(jpg_original_path,final_destination_jpg)
+        except shutil.Error as err:
+             logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err)) 
+
+    # FITS 
     fits_captures = glob(cameras_month_path[j] + '/*.fit')
+    selects = []
+
     for i in fits_captures:
+	fits_name = os.path.basename(i) + '.gz'
+	selects.append(fits_name)
 
-	# JPG
-	common_string = os.path.basename(os.path.splitext(os.path.normpath(i))[0])
-	jpg500_name = common_string + '-500x500.jpg'
-	jpg_name = common_string + '.jpg' 
-	final_destination_jpg = os.path.join(thumbs_path,jpg_name)
-	jpg_original_path = os.path.join(cameras_month_path[j],jpg500_name) 
-	if not os.path.exists(final_destination_jpg) and os.path.exists(jpg_original_path):
-	    try:
-		shutil.copy(jpg_original_path,final_destination_jpg)
-	    except shutil.Error as err:
-		logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err)) 
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool = multiprocessing.Pool(processes=thrd_nr)
+    filen  = pool.map_async(do_work, selects).get(timeout=300)
+    pool.close()
+    pool.join()
 
-        # FITS
-	fits_name = os.path.basename(i)
-	archived_capture = mysql_tools.select_EXPID(session,data_file,fits_name,logfile)
-	if not archived_capture:
-       	    try:
-	        shutil.copy(i,os.path.join(ingest_path,fits_name))
-	    except shutil.Error as err:
-	        logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
+    for i in filen:
+	    if i is not None:
+		name = os.path.splitext(os.path.normpath(i))[0]
+		path = cameras_month_path[j] + '/' + name
+		try:
+		    shutil.copy(path,os.path.join(ingest_path,name))
+	        except shutil.Error as err:
+	            logfile.write('%s -- shutil.Error: %s \n' % (datetime.now(),err))
 
 logfile.close()

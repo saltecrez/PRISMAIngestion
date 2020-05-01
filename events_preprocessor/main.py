0a73fc8fb7e6578ebcfef01935e63f84367df8ca
saltecrez <3lisa.londero@gmail.com>
2020-05-01 16:27:32 +0200

    small bug fix

--- events_preprocessor/main.py
+++ events_preprocessor/main.py
@@ -4,12 +4,14 @@ __author__ = "Elisa Londero"
 __email__ = "elisa.londero@inaf.it"
 __date__ = "June 2018"
 
+import os
 from utilities import VerifyLinux
-from event_preprocessing import SelectEventString 
-from event_preprocessing import EventPreprocessing
+from preprocessing import SelectEventString 
+from preprocessing import EventPreprocessing
 from utilities import LoggingClass
 
 log = LoggingClass('',True).get_logger()
+pwd = os.getcwd()
 
 def main():
 
@@ -23,6 +25,7 @@ def main():
             ep.copy_to_preprocessing_area()
             ep.process_event()
             ep.remove_event()
+            os.chdir(pwd)
 
     except Exception as e:
         msg = "Main exception - main() -- "
